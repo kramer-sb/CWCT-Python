11e35634dc630d260248821e69ce04bcc316fb49
Brie Kramer <48227405+kramer-sb@users.noreply.github.com>
2024-06-05 09:23:38 -0400

    Create about_python.py
    First homework for Python class with CWCT.

---
+++ about_python.py
@@ -0,0 +1,17 @@
+#First attempt, prior to class:
+#print("Q. Who was the creator of Python? A. Guido van Rossum")
+#print("Q. When was Python created? A. 1989")
+#print("Q. In the Python community, the person who created Python is commonly referred to as the \"BDFL\". What does that acronym mean? A. Benevolent dictator for life.")
+
+#I like this better but it's rather robust. 
+ansA = "Guido van Rossum created Python."
+ansB = "Python was created in 1989."
+ansC = "\"BDFL\" means benevolent dictator for life."
+
+print()
+print("Who was the creator of Python?", ansA)
+print()
+print("When was Python created?", ansB)
+print()
+print("In the Python community, the person who created Python is commonly referred to as the \"BDFL\". What does that acronym mean?", ansC)
+print()
