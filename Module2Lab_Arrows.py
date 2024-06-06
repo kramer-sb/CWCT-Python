c9b9fac948136cceaf057b2ef3ce154396ea54db
Brie Kramer <48227405+kramer-sb@users.noreply.github.com>
2024-06-06 12:08:23 -0400

    Create Module2Lab_Arrows.py
    -experimenting with existing Python code;
    -discovering and fixing basic syntax errors;
    -becoming familiar with the print() function and its formatting capabilities.
    
    Comments in code on what to do with the beginning arrow. Changes begin line 16

---
+++ Module2Lab_Arrows.py
@@ -0,0 +1,34 @@
+#take this code and edit to make two arrows, a larger arrow, play with it. 
+#Code begins line 16 for what I've done. 
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+
+
+
+
+print("    *", "        *")
+print("   * *", "      * *")
+print("  *   *", "    *   *")
+print(" *     *", "  *     *")
+print("***   ***", "***   ***")
+print("  *   *", "    *   *")
+print("  *   *", "    *   *")
+print("  *****", "    *****")
+#new
+print("      *")
+print("    *   *")
+print("   *     *")
+print("  *       *")
+print(" *         *")
+print("***       ***")
+print("   *     *")
+print("   *     *")
+print("   *     *")
+print("   *******")
