070a9728554a79a2ef767207e2b8e64fad17ee1f
Brie Kramer <48227405+kramer-sb@users.noreply.github.com>
2024-06-06 14:17:09 -0400

    Create Module2_PythonLiterals.py
    some literals from module 2

---
+++ Module2_PythonLiterals.py
@@ -0,0 +1,20 @@
+
+print("this is an octal number:", 0o123)
+
+print("this is a hexadecimal number", 0x123)
+print("The e in 3e8 equals exponent and results in:", 3e8)
+print("The E in 3E8 equals exponent and results in:", 3E8)
+print("Here, Python will print a different notation for 0.0000000000000000000001:",0.0000000000000000000001)
+print("Here we are escaping the quotes:", "I like \"Monty Python\"")
+print("But here we do not have to because we're using single and double quotations:",'I like "Monty Python"')
+#escaping in single quotes
+print('I\'m Monty Python')
+print(True < False)
+print(True > False)
+print(True < False)
+print(True > False)
+#Lab on using quotes properly
+#First single quotes, then double, then triple.
+print('"I\'m"')
+print('""learning""')
+print('"""Python"""')
