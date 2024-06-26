32dbed8f246e0bb664951eac4f3b5718d14b8ac0
Brie Kramer <48227405+kramer-sb@users.noreply.github.com>
2024-06-26 18:55:43 -0400

    Create number_guess.py
    With Python Skill Up on O'Reilly with Shaun Wassell. Random number selected and user has as many guesses as needed to guess the number. Currently set 1-10, but could go more, and could also limit the number of guesses, and adapt for even or odd numbers. Different ways to customize and keep learning with Python.

---
+++ number_guess.py
@@ -0,0 +1,19 @@
+# Number Guessing Game
+import random
+# Randomly select a number between 1 and 10
+selected_number = random.randint(1, 10)
+
+print("\nWelcome to the Number Guessing Game! I have selected a number between 1 and 10, can you guess what it is?")
+
+user_has_guessed_correctly = False
+
+while not user_has_guessed_correctly:
+    user_input = input("\nEnter your guess here: ")
+    
+    if int(user_input) == selected_number:
+        print("Congratulations! You've guessed the correct number!")
+        user_has_guessed_correctly = True
+    else:
+        print("Nope, that is not the correct number.")
+
+print("\nEnd of program")      
